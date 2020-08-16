d81291957d03f43169bcabe9b8ba625d2f5a8f5b
Anamika-11 <54811040+Anamika-11@users.noreply.github.com>
2020-08-16 21:52:32 +0530

    Add files via upload

---
+++ as.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.10.2019 17:39:50
+// Design Name: 
+// Module Name: as
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module as(
+    input a,
+    input b,
+    output c,
+    output d
+    );
+    assign c=a;
+    assign d=b;
+endmodule
